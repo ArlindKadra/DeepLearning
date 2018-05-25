0e6ffbcf0e43c095337461bf10a08bf5f7b3da1c
ArlindKadra <arlindkadra@gmail.com>
2018-05-25 15:28:50 +0200

    Trying to incorporate BOHB

--- src/main.py
+++ src/main.py
@@ -5,16 +5,25 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 
 from src.models import fcresnet
 from src.models import random_forest
+from src.models import svm
+from src.optim.hpbandster import Master
 import src.utils as utils
 
+import argparse
 import openml
 import random
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
 
-
 def main():
+
+    parser = argparse.ArgumentParser(description='HpBandSter example 2.')
+    parser.add_argument('--run_id', help='unique id to identify the HPB run.', default='HPB_example_2', type=str)
+    parser.add_argument('--array_id', help='SGE array id to tread one job array as a HPB run.', default=1, type=int)
+    parser.add_argument('--working_dir', help='working directory to store live data.', default='.', type=str)
+
+    args = parser.parse_args()
 
     logging.info('Experiment started')
     benchmark_suite = openml.study.get_study("99", "tasks")
@@ -29,8 +38,6 @@ def main():
         logging.error('Input contains NaN values')
         raise ValueError("Input contains NaN values")
 
-    # random_forest.train(x, y, categorical)
-
     if True in categorical:
         x, mean, std = utils.feature_normalization(x, categorical)
         enc = OneHotEncoder(categorical_features=categorical, dtype=np.float32)
@@ -38,10 +45,16 @@ def main():
     else:
         x, mean, std = utils.feature_normalization(x, None)
 
-    #config_space = fcresnet.get_config_space()
-    #config = config_space.sample_configuration(1)
-    #result = utils.cross_validation(x, y, fcresnet.FcResNet, config)
-    #logging.info('Cross Validation loss: %.3f, accuracy %.3f', result[0], result[1])
+    master = Master(args.run_id, args.array_id, args.working_dir)
+
+    random_forest.train(x, y, categorical)
+    svm.train(x, y, categorical)
+
+
+    # config_space = fcresnet.get_config_space()
+    # config = config_space.sample_configuration(1)
+    # result = utils.cross_validation(x, y, fcresnet.FcResNet, config)
+    # logging.info('Cross Validation loss: %.3f, accuracy %.3f', result[0], result[1])
 
 
 if __name__ == '__main__':
--- src/optim/hpbandster.py
+++ src/optim/hpbandster.py
@@ -16,58 +16,55 @@ from hpbandster.core.worker import Worker
 
 class Master():
 
-    config_space = fcresnet.get_config_space()
-    parser = argparse.ArgumentParser(description='HpBandSter example 2.')
-    parser.add_argument('--run_id', help='unique id to identify the HPB run.', default='HPB_example_2', type=str)
-    parser.add_argument('--array_id', help='SGE array id to tread one job array as a HPB run.', default=1, type=int)
-    parser.add_argument('--working_dir', help='working directory to store live data.', default='.', type=str)
+    def __init__(self, run_id, array_id, working_dir):
 
-    args = parser.parse_args()
+        config_space = fcresnet.get_config_space()
 
-    if args.array_id == 1:
-        # start nameserver
-        NS = hputil.NameServer(run_id=args.run_id, nic_name='eth0',
-                               working_directory=args.working_dir)
 
-        # stores information for workers to find in working_directory
-        ns_host, ns_port = NS.start()
+        if array_id == 1:
+            # start nameserver
+            NS = hputil.NameServer(run_id=run_id, nic_name='eth0',
+                                   working_directory=working_dir)
 
-        # BOHB is usually so cheap, that we can affort to run a worker on the master node, too.
-        worker = Slave(nameserver=ns_host, nameserver_port=ns_port, run_id=args.run_id)
-        worker.run(background=True)
+            # stores information for workers to find in working_directory
+            ns_host, ns_port = NS.start()
 
-        HPB = BOHB(configspace=config_space,
-                   run_id=args.run_id,
-                   eta=3, min_budget=1, max_budget=27,
-                   host=ns_host,
-                   nameserver=ns_host,
-                   nameserver_port=ns_port,
-                   ping_interval=3600,
-                  )
-        # BOHB can wait until a minimum number of workers is online before starting
-        res = HPB.run(n_iterations=4,
-                      min_n_workers=4
-                      )
+            # BOHB is usually so cheap, that we can affort to run a worker on the master node, too.
+            worker = Slave(nameserver=ns_host, nameserver_port=ns_port, run_id=run_id)
+            worker.run(background=True)
 
-        # pickle result here for later analysis
-        with open(os.path.join(args.working_dir, 'results.pkl'), 'wb') as fh:
-            pickle.dump(res, fh)
+            HPB = BOHB(configspace=config_space,
+                       run_id=run_id,
+                       eta=3, min_budget=1, max_budget=27,
+                       host=ns_host,
+                       nameserver=ns_host,
+                       nameserver_port=ns_port,
+                       ping_interval=3600,
+                       )
+            # BOHB can wait until a minimum number of workers is online before starting
+            res = HPB.run(n_iterations=4,
+                          min_n_workers=4
+                          )
 
-        # shutdown all workers
-        HPB.shutdown(shutdown_workers=True)
+            # pickle result here for later analysis
+            with open(os.path.join(working_dir, 'results.pkl'), 'wb') as fh:
+                pickle.dump(res, fh)
 
-        # and the nameserver
-        NS.shutdown()
+            # shutdown all workers
+            HPB.shutdown(shutdown_workers=True)
 
-    else:
+            # and the nameserver
+            NS.shutdown()
 
-        host = hputil.nic_name_to_host('eth0')
+        else:
 
-        # workers only instantiate the MyWorker, find the nameserver and start serving
-        w = Slave(run_id=args.run_id, host=host)
-        w.load_nameserver_credentials(args.working_dir)
-        # run worker in the foreground,
-        w.run(background=False)
+            host = hputil.nic_name_to_host('eth0')
+
+            # workers only instantiate the MyWorker, find the nameserver and start serving
+            w = Slave(run_id=run_id, host=host)
+            w.load_nameserver_credentials(working_dir)
+            # run worker in the foreground,
+            w.run(background=False)
 
 
 class Slave(Worker):
