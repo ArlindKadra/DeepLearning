eac53301a4889daeb4c7d23eb212c61dff8d6e64
ArlindKadra <arlindkadra@gmail.com>
2018-08-28 13:56:16 +0200

    Changes for the autosklearn experiment

--- src/autosklearn_exp.py
+++ src/autosklearn_exp.py
@@ -10,8 +10,19 @@ import argparse
 def main():
 
     parser = argparse.ArgumentParser(description='AutoSkLearn on OpenML.')
-    parser.add_argument('--run_id', help='unique id to identify the AutoSklearn run.',
-                        default='AutoSKLearn on OpenML.', type=str)
+    parser.add_argument(
+        '--run_id',
+        help='unique id to identify the AutoSklearn run.',
+        default='AutoSKLearn on OpenML.',
+        type=str
+    )
+
+    parser.add_argument(
+        '--task_id',
+        help='Task id so that the dataset can be retrieved from OpenML.',
+        default=3,
+        type=int
+    )
 
     args = parser.parse_args()
 
@@ -22,11 +33,11 @@ def main():
     utils.setup_logging("AutoSklearn" + args.run_id, logging.DEBUG if verbose else logging.INFO)
     logger.info('AutoSkLearn Experiment started')
 
-    loader = Loader(torch=False)
+    loader = Loader(task_id=args.task_id, torch=False)
     x, y, categorical = loader.get_dataset()
 
     random_forest.train(x, y, categorical)
-    #svm.train(x, y, categorical)
+    # svm.train(x, y, categorical)
 
 
 if __name__ == '__main__':
--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -7,15 +7,12 @@ from sklearn.model_selection import train_test_split
 def train(x, y, categorical_indicator):
 
     logger = logging.getLogger(__name__)
-    logger.info('Bla')
-    b = 1.5
-    logger.info('Random fores score %f %%', b)
     # Create feature type list indicator and run autosklearn
     feat_type = ['Categorical' if feature else 'Numerical'
                  for feature in categorical_indicator]
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1 / 10)
     random_forest = RandomForest().build()
-    random_forest.fit(x_train, y_train, feat_type=feat_type)
+    random_forest.fit(x_train, y_train, x_test, y_test, feat_type=feat_type)
     y_prediction = random_forest.predict(x_test)
     logger.info('Random Forest accuracy score: %.3f %%', (accuracy_score(y_test, y_prediction)).item())
 
@@ -31,5 +28,5 @@ class RandomForest(object):
 
         return autosklearn.classification.AutoSklearnClassifier(
             include_estimators=["random_forest"], ensemble_size=1,
-            tmp_folder="autosklearn_exp/output", delete_tmp_folder_after_terminate=False,
+            output_folder="autosklearn_exp/output", delete_output_folder_after_terminate=False,
             initial_configurations_via_metalearning=0)
